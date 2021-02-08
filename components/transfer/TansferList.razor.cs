6c136dc41bdcfb5500958b86bf8a4d7912a85f3e
ElderJames <shunjiey@hotmail.com>
2021-02-08 23:59:18 +0800

    refactor(module: transfer): add transfer list component

---
+++ components/transfer/TansferList.razor.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace AntDesign
+{
+    public partial class TansferList
+    {
+        [CascadingParameter]
+        private Transfer Transfer { get; set; }
+
+        [Parameter]
+        public TransferDirection Direction { get; set; }
+
+        [Parameter]
+        public string TitleText { get; set; }
+
+        [Parameter]
+        public bool ShowSelectAll { get; set; }
+
+        [Parameter]
+        public string ItemUnit { get; set; }
+
+        [Parameter]
+        public string ItemsUnit { get; set; }
+
+        [Parameter]
+        public string Filter { get; set; }
+
+        [Parameter]
+        public bool Disabled { get; set; }
+
+        [Parameter]
+        public bool ShowSearch { get; set; }
+
+        [Parameter]
+        public string SearchPlaceholder { get; set; }
+
+        [Parameter]
+        public string NotFoundContent { get; set; }
+
+        /// <summary>
+        /// (inputValue: string, item: TransferItem) => boolean
+        /// </summary>
+        [Parameter]
+        public Func<string, TransferItem, bool> FilterOption { get; set; }
+
+        [Parameter]
+        public IEnumerable<TransferItem> DataSource { get; set; }
+
+        [Parameter]
+        public RenderFragment RenderList { get; set; }
+
+        [Parameter]
+        public RenderFragment Render { get; set; }
+
+        [Parameter]
+        public string Footer { get; set; } = string.Empty;
+
+        [Parameter]
+        public RenderFragment FooterTemplate { get; set; }
+
+        [Parameter]
+        public EventCallback<bool> HandleSelectAll { get; set; }
+
+        [Parameter]
+        public EventCallback<TransferItem> HandleSelect { get; set; }
+
+        [Parameter]
+        public EventCallback<(string direction, string value)> FilterChange { get; set; }
+
+        private string _filterValue;
+
+        private record Stat(bool CheckAll, bool CheckHalf, int CheckCount, int ShownCount);
+
+        private Stat _stat = new(false, false, 0, 0);
+
+        private async Task HandleSearch(ChangeEventArgs e, string direction, bool mathTileCount = true)
+        {
+            _filterValue = e.Value.ToString();
+            DataSource = DataSource.Where(a => !_targetKeys.Contains(a.Key) && a.Title.Contains(_leftFilterValue, StringComparison.InvariantCultureIgnoreCase)).ToList();
+
+            if (mathTileCount)
+                MathTitleCount();
+
+            //if (OnSearch.HasDelegate)
+            //{
+            //    await OnSearch.InvokeAsync(new TransferSearchArgs(direction, e.Value.ToString()));
+            //}
+        }
+
+        private async Task ClearFilterValueAsync(string direction)
+        {
+            if (direction == TransferDirection.Left)
+            {
+                _leftFilterValue = string.Empty;
+                await HandleSearch(new ChangeEventArgs() { Value = string.Empty }, direction);
+            }
+            else
+            {
+                _rightFilterValue = string.Empty;
+                await HandleSearch(new ChangeEventArgs() { Value = string.Empty }, direction);
+            }
+        }
+
+        private void MathTitleCount()
+        {
+            _rightButtonDisabled = _sourceSelectedKeys.Count == 0;
+            _leftButtonDisabled = _targetSelectedKeys.Count == 0;
+
+            var leftSuffix = _leftDataSource.Count() == 1 ? Locale.ItemUnit : Locale.ItemsUnit;
+            var rightSuffix = _rightDataSource.Count() == 1 ? Locale.ItemUnit : Locale.ItemsUnit;
+
+            var leftCount = _sourceSelectedKeys.Count == 0 ? $"{_leftDataSource.Count()}" : $"{_sourceSelectedKeys.Count}/{_leftDataSource.Count()}";
+            var rightCount = _targetSelectedKeys.Count == 0 ? $"{_rightDataSource.Count()}" : $"{_targetSelectedKeys.Count}/{_rightDataSource.Count()}";
+
+            _leftCountText = $"{leftCount} {leftSuffix}";
+            _rightCountText = $"{rightCount} {rightSuffix}";
+
+            CheckAllState();
+        }
+    }
+}
--- components/transfer/Transfer.razor.cs
+++ components/transfer/Transfer.razor.cs
@@ -67,10 +67,10 @@ namespace AntDesign
 
         private List<string> _targetKeys;
 
-        private bool _leftCheckAllState = false;
-        private bool _leftCheckAllIndeterminate = false;
-        private bool _rightCheckAllState = false;
-        private bool _rightCheckAllIndeterminate = false;
+        private bool _leftCheckAllState;
+        private bool _leftCheckAllIndeterminate;
+        private bool _rightCheckAllState;
+        private bool _rightCheckAllIndeterminate;
 
         private string _leftCountText = string.Empty;
         private string _rightCountText = string.Empty;
@@ -98,31 +98,12 @@ namespace AntDesign
             var count = _sourceSelectedKeys.Count;
 
             InitData();
-
-            MathTitleCount();
         }
 
         private void InitData()
         {
             _leftDataSource = DataSource.Where(a => !_targetKeys.Contains(a.Key));
             _rightDataSource = DataSource.Where(a => _targetKeys.Contains(a.Key));
-        }
-
-        private void MathTitleCount()
-        {
-            _rightButtonDisabled = _sourceSelectedKeys.Count == 0;
-            _leftButtonDisabled = _targetSelectedKeys.Count == 0;
-
-            var leftSuffix = _leftDataSource.Count() == 1 ? Locale.ItemUnit : Locale.ItemsUnit;
-            var rightSuffix = _rightDataSource.Count() == 1 ? Locale.ItemUnit : Locale.ItemsUnit;
-
-            var leftCount = _sourceSelectedKeys.Count == 0 ? $"{_leftDataSource.Count()}" : $"{_sourceSelectedKeys.Count}/{_leftDataSource.Count()}";
-            var rightCount = _targetSelectedKeys.Count == 0 ? $"{_rightDataSource.Count()}" : $"{_targetSelectedKeys.Count}/{_rightDataSource.Count()}";
-
-            _leftCountText = $"{leftCount} {leftSuffix}";
-            _rightCountText = $"{rightCount} {rightSuffix}";
-
-            CheckAllState();
         }
 
         private async Task SelectItem(bool isCheck, string direction, string key)
@@ -138,8 +119,6 @@ namespace AntDesign
                 holder.Add(key);
 
             HandleSelect(direction, holder);
-
-            MathTitleCount();
 
             if (OnSelectChange.HasDelegate)
             {
@@ -158,8 +137,6 @@ namespace AntDesign
             var holder = isCheck ? list.Where(a => !a.Disabled).Select(a => a.Key).ToList() : new List<string>(list.Count());
 
             HandleSelect(direction, holder);
-
-            MathTitleCount();
 
             if (OnSelectChange.HasDelegate)
             {
@@ -190,7 +167,6 @@ namespace AntDesign
             else
             {
                 _targetKeys.AddRange(moveKeys);
-
             }
 
             InitData();
@@ -199,17 +175,15 @@ namespace AntDesign
 
             HandleSelect(oppositeDirection, new List<string>());
 
-            if (!string.IsNullOrEmpty(_leftFilterValue))
-            {
-                await HandleSearch(new ChangeEventArgs() { Value = _leftFilterValue }, TransferDirection.Left, false);
-            }
+            //if (!string.IsNullOrEmpty(_leftFilterValue))
+            //{
+            //    await HandleSearch(new ChangeEventArgs() { Value = _leftFilterValue }, TransferDirection.Left, false);
+            //}
 
-            if (!string.IsNullOrEmpty(_rightFilterValue))
-            {
-                await HandleSearch(new ChangeEventArgs() { Value = _rightFilterValue }, TransferDirection.Right, false);
-            }
-
-            MathTitleCount();
+            //if (!string.IsNullOrEmpty(_rightFilterValue))
+            //{
+            //    await HandleSearch(new ChangeEventArgs() { Value = _rightFilterValue }, TransferDirection.Right, false);
+            //}
 
             if (OnChange.HasDelegate)
             {
@@ -247,42 +221,6 @@ namespace AntDesign
             if (OnScroll.HasDelegate)
             {
                 await OnScroll.InvokeAsync(new TransferScrollArgs(direction, e));
-            }
-        }
-
-        private async Task HandleSearch(ChangeEventArgs e, string direction, bool mathTileCount = true)
-        {
-            if (direction == TransferDirection.Left)
-            {
-                _leftFilterValue = e.Value.ToString();
-                _leftDataSource = DataSource.Where(a => !_targetKeys.Contains(a.Key) && a.Title.Contains(_leftFilterValue, StringComparison.InvariantCultureIgnoreCase)).ToList();
-            }
-            else
-            {
-                _rightFilterValue = e.Value.ToString();
-                _rightDataSource = DataSource.Where(a => _targetKeys.Contains(a.Key) && a.Title.Contains(_rightFilterValue, StringComparison.InvariantCultureIgnoreCase)).ToList();
-            }
-
-            if (mathTileCount)
-                MathTitleCount();
-
-            if (OnSearch.HasDelegate)
-            {
-                await OnSearch.InvokeAsync(new TransferSearchArgs(direction, e.Value.ToString()));
-            }
-        }
-
-        private async Task ClearFilterValueAsync(string direction)
-        {
-            if (direction == TransferDirection.Left)
-            {
-                _leftFilterValue = string.Empty;
-                await HandleSearch(new ChangeEventArgs() { Value = string.Empty }, direction);
-            }
-            else
-            {
-                _rightFilterValue = string.Empty;
-                await HandleSearch(new ChangeEventArgs() { Value = string.Empty }, direction);
             }
         }
     }
